577c0e4e1bf667cf589432f767e5583891ffe90a
Sam WSL <sam.ds@live.com>
2021-10-20 11:09:41 +0200

    polymorphic args list

--- testing/prova.ml
+++ testing/prova.ml
@@ -1,11 +1,17 @@
-open Lists
-open Sorting
-
 let l1 = [2;2;7;25;3;11;-1;0;7;25;25;0;99;-25;7](* *)
 let l2 =[4;8;7;5;2;1;3;4;3;8]
 
 
+open Varargs
 
+module T = struct type t = int end
 
+module P = Varargs.VarArgs(Varargs.ListConcat(T))
+
+let s = P.f (P.arg 1) (P.arg 2) P.stop;;
+
+open Lists
+
+Lists.MyLists.print_list s
 
 ;;exit 0
--- testing/varargs.ml
+++ testing/varargs.ml
@@ -1,6 +1,26 @@
-let arg = fun y f->(f (y))
-let s x = x
-let init = 0
+module type OpVarArgs = 
+  sig
+    type a
+    type b
+    type c
+    val op: a -> b -> c
+    val init : c
+  end
 
-let f g = g 0
+module VarArgs = functor (Op : OpVarArgs) -> struct
+  let arg a1 = fun a2 f -> f (Op.op a1 a2)
+  let stop x = x
 
+  let f g = g Op.init
+
+end
+
+module type ListType = sig
+  type t
+end
+(** Polymorphic list var args *)
+module ListConcat (T: ListType) = struct
+  type a = T.t and b = a list and c = a list
+  let init = []
+  let op = fun e l -> l @ [e]
+end
