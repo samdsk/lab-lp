61f089583e2156cd27f3091704e588c610fca78f
Sam WSL <sam.ds@live.com>
2021-10-22 16:12:09 +0200

    print tree

--- testing/dfs_bfs.ml
+++ testing/dfs_bfs.ml
@@ -1,7 +1,6 @@
 open Graphs
 
-let arcs_1 = [(1,2);(1,3);(4,1);(5,4);(3,2);(2,5);(5,3);(5,6);(5,7);(6,7);(6,3);(6,4)]
-let arcs_2 = [("Algol","Pascal");("Algol","C");("Algol","Java");("C","Java");("Algol","Python");("Pascal","Modula 2");("C","C++");("Java","Scala");("Lisp","ML");("Lisp","Scale");("Lisp","Erlang");("ML","OcaML")]
+
 
 let arcs_to_graph arcs = 
   let rec arcs_to_graph g = function
@@ -37,4 +36,28 @@ let bfs graph v =
   in  if(Graphs.Graph.is_empty graph) then raise Graphs.Graph.TheGraphIsEmpty
       else if not(Graphs.Graph.node_is_in_graph v graph) then raise Graphs.Graph.TheNodeIsNotInGraph
       else graph_to_tree(bfs graph v (Graphs.Graph.add_node v (Graphs.Graph.empty())) (Graphs.Graph.adjacents v graph)) v
+  
+  let rec print_tlist = function
+      | (Tree(v,_))::t -> print_string(v^" "); print_tlist t
+      | Leaf(v)::t -> print_string(v^" ");print_tlist t
+      | [] -> print_string("\n")
+ 
 
+let rec print_tree = function
+  | Leaf(v) -> ()(*print_string(v^"-leaf\n")*)
+  | Tree(v,tl) -> print_string(v^"\n"); (print_tlist tl); 
+      let rec print_bfs l = match l with
+        | [] -> () (*print_string("-\n")*)
+        | h::t -> print_tree h; print_bfs t
+      in print_bfs tl
+  
+
+let arcs_1 = [(1,2);(1,3);(4,1);(5,4);(3,2);(2,5);(5,3);(5,6);(5,7);(6,7);(6,3);(6,4)]
+let arcs_2 = [("Algol","Pascal");("Algol","C");("Algol","Java");("C","Java");("Algol","Python");("Pascal","Modula 2");("C","C++");("Java","Scala");("Lisp","ML");("Lisp","Scale");("Lisp","Erlang");("ML","OcaML")]
+
+
+let g_1 = arcs_to_graph arcs_2
+
+let t_1 = dfs g_1 "Algol"
+
+let () = print_tree t_1 
