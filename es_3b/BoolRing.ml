f94b9469635eae7d249f82979cf5a1d5a993d55f
SamDs <sam.ds@live.com>
2021-10-23 11:38:58 +0200

    reset 2021-2022

--- es_3b/BoolRing.ml
+++
@@ -1,9 +0,0 @@
-module BoolRing =
-  struct
-    type t = bool
-    let zero = false
-    let one = true
-    let (+) x y = x || y
-    let ( * ) x y = x && y
-    let pp_print ppf e = Format.fprintf ppf "%B" e
-  end
--- testing/varargs.ml
+++
@@ -1,6 +0,0 @@
-let arg = fun y f->(f (y))
-let s x = x
-let init = 0
-
-let f g = g 0
-
