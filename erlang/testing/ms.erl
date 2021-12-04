e39c9c33df5924b1835108ea9a18c76075272c85
Sam WSL <sam.ds@live.com>
2021-12-04 20:38:24 +0100

    macros

--- erlang/testing/ms.erl
+++ erlang/testing/ms.erl
@@ -1,5 +1,14 @@
 -module(ms).
 -export([s/0]).
+-define(debug,true).
 
 
+-ifdef(debug).
+-define(prova,x).
+-endif.
 
+-ifdef(prova).
+s() -> io:format(?prova).
+-else.
+s() -> io:format("no\n").
+-endif.
