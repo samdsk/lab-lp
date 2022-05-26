0410279d6e9c84392f47b532e6d56cfba98292c7
SamDs <sam.ds@live.com>
2022-05-26 20:17:58 +0200

    editor

--- scala/scala_basic/scala_8_Playing with Traits/es8.scala
+++ scala/scala_basic/scala_8_Playing with Traits/es8.scala
@@ -1,34 +1,67 @@
+import java.lang.StringBuilder
 
 class editor{
-    val line :List[Char]
-    private var cursor_position : Int
+    val line : StringBuilder = new StringBuilder("")
+    private var cursor_position : Int = 0
+    
 
-    def x = {
-
+    def x = line(cursor_position) match{
+        case _ if line.isEmpty => 
+        case _ => {
+            list.deleteCharAt(cursor_position);
+            if(check(1,-)) cursor_position += 1
+        }
     }
 
     def dw = {
-
+        val next_space = list.indexOf(' ', cursor_position)
+        if(next_space >=0 ){
+            list.delete(cursor_position, next_space-1)
+            cursor_position = next_space
+        }else{
+            list.delete(cursor_position, list.length -1)
+            if(check(1, -)) cursor_position -=1
+            else cursor_position = 0
+        }
     }
 
-    def i(c : Char) = {
+    def i(c : Char) = {        
+        if(check(1, +)){
+            list.insert(cursor_position,c)
+        }else{
+            list.append(c)
+            cursor_position = list.length - 1
+        }
         
     }
 
+    def iw(s : String) = {
+        if(check(1, +)){
+                list.insert(cursor_position+1,s + " ")
+            }else{
+                list.append(s + " ")
+                cursor_position = list.length - 1
+            }
+    }
+
     def l(n : Int = 1) = {
-        if(!check(cursor_position+n)) return
+        if(!check(n, + )) return
         cursor_position += n
     }
 
     def h(n : Int = 1) = {
-        if(!check(cursor_position-n)) return
+        if(!check(n, - )) return
         cursor_position -= n
     }
 
-    def check(n : Int) : Boolean = {
+    def check(n : Int, op : (Int,Int) => Int ) : Boolean = {
         if(line.isEmpty) false
-        if(line.length - 1 < n) false
+        if(line.length <= (cursor_position op n)) false
         else true
+    }
+
+    def check : Boolean = {
+        check(0, + )
     }
 
 }
