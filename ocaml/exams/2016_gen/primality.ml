81d36fc2c8a4e1aa7af43cd8fbe5c20126c26bc9
SamDs <sam.ds@live.com>
2022-11-13 17:31:00 +0100

    added comments

--- ocaml/exams/2016_gen/primality.ml
+++ ocaml/exams/2016_gen/primality.ml
@@ -5,23 +5,27 @@ let trialdivision n =
     | d -> check x (if d=2 then 3 else (d+2))
 in  check n 2
 
+(** Lucas-Lehmer prime test - M_p = 2^p - 1 -> p = log(m+1)/log(2) *)
 let lucaslehmer m =
   let rec s_i s_i_1 = function
     | 0 -> s_i_1 = 0.
     | p -> s_i ((s_i_1 ** 2.) -. 1.) (p-1) in s_i 4. (int_of_float((log (float m +. 1.)) /. (log 2.) -. 2.))
 
+(**produce a list of int within the given range and step excluding bounds*)
 let range ~start:i ~finish:e ~step:step =
   let rec range s f acc = match f with
       | f when f=s -> acc
       | _ -> range (s+step) f (s+step::acc)
 in range i e []
 
+(** Modular exponential resolver a^e = b (mod m) *)
 let mod_resolve a e m = 
   let rec find b = function
     | l when l==e -> b
     | l -> find (b*a mod m) (l+1)
 in find 1 0
 
+(**Little Fermat prime test*)
 let littlefermat n = 
   let n_1 = n-1 in
     let a = (range ~start:0 ~finish:(int_of_float (log (float n))) ~step:1) in
