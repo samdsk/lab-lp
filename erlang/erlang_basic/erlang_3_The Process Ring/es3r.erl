abba564c1df767b7864c7b28e8d8e4d4811d696d
SamDs <sam.ds@live.com>
2022-12-03 22:23:30 +0100

    es3 recursive version

--- erlang/erlang_basic/erlang_3_The Process Ring/es3r.erl
+++ erlang/erlang_basic/erlang_3_The Process Ring/es3r.erl
@@ -3,26 +3,21 @@
 
 
 start(M,N,Message) ->
+    unregister(last),
     register(last,spawn(?MODULE,ring,[N,self()])),
     receive
         {ok} -> last ! {M,msg,Message}
 end.
 
 ring(1,Start) ->
-    %%io:format("Ok ring created\n"),
     Start ! {ok},
     listen(1,whereis(last));
-ring(N,Start) ->
-    %%io:format("ring:~p(~p)~n",[N,self()]), 
+ring(N,Start) ->    
     Next = spawn(?MODULE,ring,[N-1,Start]),
     listen(N,Next).
 
 listen(1 = N,Next) -> 
-    %%io:format("listening: ~p next:~p\n",[N,Next]),
     receive
-        % {pid,Pid} ->  
-        %     io:format("Setting up next:~p(~p) - ~p\n",[N,self(),Pid]),
-        %     listen(N,Pid);
         {0,msg,Msg} ->  
             io:format("Loop finished: ~p\n",[Msg]),
             listen(N,Next);
@@ -37,11 +32,7 @@ listen(1 = N,Next) ->
 end;
 
 listen(N,Next) -> 
-    %%io:format("listening: ~p next:~p\n",[N,Next]),
     receive
-        {0,msg,Msg} ->  
-            io:format("Loop finished: ~p\n",[Msg]),
-            listen(N,Next);
         {M,msg,Msg}=Message ->    
             io:format("ps:~p(~p)- M:~p - Msg: ~p~n",[N,self(),M,Msg]),
             Next ! Message,
