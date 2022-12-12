dd45298ee106c55f69b7ebe48a46965c19968945
SamDs <sam.ds@live.com>
2022-12-12 20:28:26 +0100

    fixed, improved make_node and ping

--- erlang/exams/2021_dic/mm.erl
+++ erlang/exams/2021_dic/mm.erl
@@ -1,11 +1,10 @@
 -module(mm).
 -export([start/1]).
--define(Shell,whereis(shell)).
 -define(server,client:make_node("server")).
 
 start(MM)->
     register(mm,self()),
-    io:format(?Shell,"~p: up and running at ~p\n",[MM,node()]),
+    io:format(user,"~p: up and running at ~p\n",[MM,node()]),
     listen(MM).
 
 listen(MM) ->
@@ -13,6 +12,6 @@ listen(MM) ->
         {cmd,spawn_server} -> spawn(?server,server,start,[]), listen(MM);
         {cmd,reverse_ready,_} = Msg -> {server,?server} ! Msg, listen(MM);
         {reverse,_} = Msg -> 
-            io:format(?Shell,"~p received from client ~p\n",[node(),Msg]),
+            io:format(user,"~p received from client ~p\n",[node(),Msg]),
             {server,?server} ! {MM,Msg}, listen(MM)
 end.
--- erlang/exams/2021_dic/server.erl
+++ erlang/exams/2021_dic/server.erl
@@ -1,28 +1,29 @@
 -module(server).
 -export([start/0]).
--define(Shell,whereis(shell)).
 
 start() ->
     register(server,self()),
-    io:format(?Shell,"Server: up and running at ~p\n",[node()]),
+    io:format(user,"Server: up and running at ~p\n",[node()]),
     listen().
 
 listen() ->
-    io:format(?Shell,"Server ready!\n",[]),
+    io:format(user,"Server ready!\n",[]),
     receive
         {cmd,reverse_ready,Len} -> 
-            io:format(?Shell,"Entering collection mode to perfome reverse operation\n",[]),
-            do_reverse(Len*2,[],[]), listen()
+            io:format(user,"Entering collection mode to perfome reverse operation\n",[]),
+            {client,client:make_node("client")} ! {do_reverse(Len*2,[],[])},
+            listen()
 end.
 
 do_reverse(0,L1,L2) ->
     L = L2++L1,
-    io:format(?Shell,"The reverse of ~p is ~p\n",[lists:reverse(L),L]);
+    io:format(user,"The reverse of ~p is ~p\n",[lists:reverse(L),L]),
+    L;
 
 do_reverse(Len,L1,L2) ->
     receive 
     {MM,{reverse,{N,L,H}}} -> 
-            io:format(?Shell,"Msg#reverse -> from ~p - (~p of ~p) :- ~p\n",[MM,N,L,H]), 
+            io:format(user,"Msg#reverse -> from ~p - (~p of ~p) :- ~p\n",[MM,N,L,H]), 
             case MM of
                 mm1 -> do_reverse(Len-1,[H|L1],L2);
                 _ -> do_reverse(Len-1,L1,[H|L2])
