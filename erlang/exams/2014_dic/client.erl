39aa93e3b268e83cdee7a420096161c3172b2adf
SamDs <sam.ds@live.com>
2022-12-13 19:35:52 +0100

    december 2014

--- erlang/exams/2014_dic/client.erl
+++ erlang/exams/2014_dic/client.erl
@@ -1,15 +1,28 @@
 -module(client).
--export([is_prime/1,close/0]).
+-export([is_prime/1,close/0,node_addr/1,ping/0]).
+% -compile(export_all).
+
+ping() -> net_adm:ping(node_addr("sif")).
+
+node_addr(Name) ->
+    {_,Host} = inet:gethostname(),
+    list_to_atom(Name++[$@]++Host).
+
+rpc(Msg) ->
+    register(client,self()),
+    {server,node_addr("sif")} ! Msg,
+    receive
+        {result,{N,uncheckable}} -> 
+            io:format(user,"~p is uncheckable\n",[N]);
+        {result,{N,R}} -> 
+            io:format(user,"is ~p prime? ~p\n",[N,R])
+    after 5000 ->
+        io:format(user,"Client timeout\n",[]) 
+end.
 
 is_prime(N) ->
-    global:register_name(client, 
-        spawn(fun()-> 
-            global:send(server,{new,N}),
-                receive
-                    {result,R} -> io:format("is ~p prime? ~p\n",[N,R]);
-                    Any -> io:format("un: ~p",[Any])
-                end
-        end)
-    ).
+    rpc({new,N}).
 
-close() -> global:send(server,quit).
+close() ->
+    rpc({quit}).
+
--- erlang/exams/2014_dic/controller.erl
+++ erlang/exams/2014_dic/controller.erl
@@ -1,19 +1,38 @@
 -module(controller).
--export([start/1]).
+-export([start/1,node_addr/1]).
+% -compile(export_all).
+-define(Client,controller:node_addr("amora")).
 
+node_addr(Name) ->
+    {_,Host} = inet:gethostname(),
+    list_to_atom(Name++[$@]++Host).
 
-start(Limit) ->
-    global:register_name(server,
-    spawn(fun() -> 
-        process_flag(trap_exit, true),
-        Sieve_1 = spawn_link(sieve,start,[2,Limit]),
-        listen(Sieve_1,Limit)
-end)).
+start(N) ->
+    Server = spawn(fun() -> 
+        process_flag(trap_exit,true),
+        Primes = [X || X <- lists:seq(2, N), length([Y || Y <- lists:seq(2,trunc(math:sqrt(X))), (X rem Y) =:= 0])=:=0],
+        First = spawn_link(sieve,start,[Primes]),
+        register(first,First),
+        Limit = lists:last(Primes),        
+        server(Limit*Limit) end),
+    register(server,Server).
 
-listen(Sieve_1,Limit) ->
-    receive
-        {new,N} when N > Limit -> io:format("~p is uncheckable, too big value.~n",[N]), listen(Sieve_1,Limit);
-        {new,N} = Msg -> io:format("You asked for: ~p~n",[N]), Sieve_1 ! Msg, listen(Sieve_1,Limit);
-        {res,R} -> global:send(client,{result,R}), listen(Sieve_1,Limit);
-        quit -> io:format("I'm closing ...~n"), exit(closing)
+server(Limit) ->
+    io:format(user,"Server up and running at ~p - server limit: ~p\n",[self(),Limit]),
+    receive 
+        {new,N} = M when N < Limit -> 
+            io:format(user,"You asked for: ~p\n",[N]),
+            first ! M, server(Limit);
+        {new,N} -> 
+            io:format(user,"You asked for: ~p\n",[N]),
+            {client,?Client} ! {result,{N,uncheckable}}, server(Limit); 
+        {quit} ->
+            io:format(user,"I'm closing...\n",[]),
+            unregister(server),
+            unregister(first),
+            exit(normal);
+        {res,R} ->  
+            io:format(user,"Server received result : ~p\n",[R]),
+            {client,?Client} ! {result,R}, server(Limit);
+        Any -> io:format(user,"Server received unknown cmd : ~p\n",[Any])
 end.
