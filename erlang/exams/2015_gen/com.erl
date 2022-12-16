c15934aa7ed1d5e40b154a03d148b8bef20c3506
SamDs <sam.ds@live.com>
2022-12-16 17:59:39 +0100

    gen15

--- erlang/exams/2015_gen/com.erl
+++ erlang/exams/2015_gen/com.erl
@@ -4,7 +4,37 @@
 
 start(N,M) ->
     process_flag(trap_exit, true),
-    Workers = [spawn_link(gen,start,[X,M])|| X<-lists:seq(0, N-1)],
-    listen(Workers).
+    Workers = [spawn_link(gene,start,[X,N,M])|| X<-lists:seq(0, N-1)],
+    permutations(Workers,N-1).
 
-permutations([H|Workers]) -> 
+permutations(Workers,Max) ->     
+    send(Workers),
+    collect(0,Max,[]),
+    exit(normal).
+
+
+
+send([]) -> ok;
+send([H|Workers]) ->
+    H ! {gen,self()},
+    send(Workers).
+
+collect(Pos,Max,Output) when Pos > Max ->
+    lists:foreach(fun(X) -> print(X) end,Output);
+collect(Pos,Max,Output) -> 
+    receive
+        {res,P,Res} when Pos =:= P -> 
+            % io:format("Found P:~p\n",[P]),            
+            collect(Pos+1,Max,build(Output, Res,[]))
+end. 
+
+build([],[],Acc) -> lists:reverse(Acc);
+build([],L,_Acc) -> 
+    lists:map(fun(X) -> [X] end, L);
+build([OH|Output],[H|Res],Acc) ->    
+    build(Output,Res,[[H]++OH]++Acc).
+
+print([H]) -> io:format("~p\n",[H]);
+print([H|T]) ->
+    io:format("~p,",[H]),
+    print(T).
